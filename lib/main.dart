74b627522ed3697ec3a18e2740064d69c76eabcc
mahadi-nsu <mahadi.hassan@northsouth.edu>
2020-03-12 11:47:32 +0600

    product grid and detail screen added along with navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/products_overview_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,9 +10,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MyShop',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
-      home: MyHomePage(),
+      home: ProductOverviewScreen(),
     );
   }
 }
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:shoppingcart/screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  const ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: (
+        GridTile(
+        child: GestureDetector(
+          onTap:(){
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (ctx)=>ProductDetailScreen(title))
+            );
+          } ,
+            child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          backgroundColor: Colors.black87,
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+        ),
+      )),
+    );
+  }
+}
